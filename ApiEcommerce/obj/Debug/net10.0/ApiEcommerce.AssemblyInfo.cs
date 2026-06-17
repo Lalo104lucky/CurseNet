041ccaa341c53ee660f6e48125d1c175e3f9e01c
Lalo104lucky <20223tn021@utez.edu.mx>
2026-06-17 14:07:19 -0600

    [ADD] GET, POST, GETBYID from Users

--- ApiEcommerce/obj/Debug/net10.0/ApiEcommerce.AssemblyInfo.cs
+++ ApiEcommerce/obj/Debug/net10.0/ApiEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c5e15a4dd05e98a02614e45c2b2f7fc4e296ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2818b5f090a9c3c4a8aca1aeabf478d1b6746f5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
