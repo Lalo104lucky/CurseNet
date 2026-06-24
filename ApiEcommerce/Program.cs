423197976d2f310c70dd8279a2b38cb22b7206e5
Lalo104lucky <20223tn021@utez.edu.mx>
2026-06-23 18:32:42 -0600

    [ADD] Authentication, first part

--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -3,6 +3,9 @@ using ApiEcommerce.Repository.IRepository;
 using ApiEcommerce.Mapping;
 using Microsoft.EntityFrameworkCore;
 using ApiEcommerce.Constants;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,29 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddAutoMapper(_ => { }, typeof(Program).Assembly);
+
+var secretKey = builder.Configuration.GetValue<string>("ApiSettings:SecretKey");
+if (string.IsNullOrEmpty(secretKey))
+{
+    throw new InvalidOperationException("SecretKey no está configurada");
+}
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+        ValidateIssuer = false,
+        ValidateAudience  = true
+    };
+});
+
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
